e5fec91e97442abf92724be322582bafb1723371
niscolas <nic.cp@hotmail.com>
2022-05-27 11:46:59 -0300

    i3, kitty, xrandr, rofi and polybar updates

--- rofi/scripts/test.sh
+++ rofi/scripts/test.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-vari="a"
-[ -n "$(pgrep kitty)" ] && echo "hello"
+if [ $@ == "shutdown" ]; then
+    poweroff
+    exit 0
+elif [[ $@ == "restart" ]]; then
+    reboot
+    exit 0
+elif [[ $@ == "logout" ]]; then
+    pkill -u niscolas
+    exit 0
+elif [[ $@ == "lock" ]]; then
+    exit 0
+elif [[ $@ == "suspend" ]]; then
+    systemctl suspend
+    exit 0
+fi
+
+echo "shutdown"
+echo "restart"
+echo "logout"
+echo "lock"
+echo "suspend"
---
+++ xrandr/hdmi_only.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xrandr --output HDMI-0 --primary --mode 1920x1080 --rate 120 --output eDP-1-1 --off
